283e2c8ecb3262c3a1efa530a535a32b85dfa830
Diego Vilhalva <diegorocha450@yahoo.com>
2025-01-20 13:46:34 -0300

    search component added

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -39,3 +39,18 @@ export const getProductsByCategorySlug = async (slug: string) => {
   const products = await sanityFetch({ query: query, params: { slug } });
   return products.data as Product[];
 }
+
+
+export const searchProducts = async(searchQuery:string) => {
+  const query = `*[_type == "product" && (
+    title match "*" + $searchQuery + "*" ||
+    description match "*" + $searchQuery + "*" ||
+    category->title match "*" + $searchQuery + "*" ||
+    category->slug.current match "*" + $searchQuery + "*"
+  )]`;
+
+
+  const products = await sanityFetch({query:query,params:{searchQuery}})
+
+  return products.data as Product[]
+}
